47f8404b2d862c2cb0a0026979a7a35475fb40fe
spirquel_samsung <t.tessier2002@gmail.com>
2025-04-27 21:28:22 +0200

    #ARDUINO# [FEatures]: -Upgrade led up down (first tuto)

--- Arduino_wd/src/01_up_down_led/01_up_down_led.ino
+++ Arduino_wd/src/01_up_down_led/01_up_down_led.ino
@@ -1,12 +1,23 @@
+//broche number
+const int xi_RedLed = 2;
+
+void UpDown(const int led, const int sleep_time)
+{
+  digitalWrite(led, HIGH); //up led (like true)
+  delay(sleep_time); //ms
+  digitalWrite(led, LOW); //down led (like false)
+  delay(sleep_time); //ms
+}
+
+//code execution one time
 void setup() {
-  // code execution one time:
+//pin led in card (OUPUT: SET DATA)
   pinMode(LED_BUILTIN, OUTPUT);
+//pin led in breadboard (INPUT: GET DATA)
+  pinMode(xi_RedLed, OUTPUT);
 }
 
 void loop() {
-  // boucle:
-  digitalWrite(LED_BUILTIN, HIGH); //up led
-  delay(500); //ms
-  digitalWrite(LED_BUILTIN, LOW); //down led
-  delay(500); //ms
+  UpDown(LED_BUILTIN, 500);
+  UpDown(xi_RedLed, 500);
 }
