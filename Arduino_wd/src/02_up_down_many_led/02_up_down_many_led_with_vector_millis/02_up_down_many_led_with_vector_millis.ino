4450fcef81a178377a1abca4f54b22216aa9608b
spirquel_samsung <t.tessier2002@gmail.com>
2025-04-29 22:32:24 +0200

    #ARDUINO# [Features]: - Correction of last code for 02 folder; the problem is because the if are not write with '{}'; add a digitalWrite for all led start with HIGH

--- Arduino_wd/src/02_up_down_many_led/02_up_down_many_led_with_vector_millis/02_up_down_many_led_with_vector_millis.ino
+++ Arduino_wd/src/02_up_down_many_led/02_up_down_many_led_with_vector_millis/02_up_down_many_led_with_vector_millis.ino
@@ -1,5 +1,5 @@
 long old_time;
-boolean led_status;
+bool led_status;
 int Nb_Pin_Leds=4;
 int idx_led_loop;
 int LedsPin[]={5, 4, 3, 2};
@@ -11,17 +11,26 @@ void setup()
     pinMode(LedsPin[idx_led], OUTPUT);
   }
   idx_led_loop=0;
+  led_status = true;
   old_time = millis();
 }
 
 void loop()
 {
   digitalWrite(LedsPin[idx_led_loop], led_status);
-  delay(500);
-  led_status = !led_status;
+  if((millis() - old_time) >= 500)
+    led_status = !led_status;
   
   if((millis() - old_time) > 1000)
-    idx_led_loop++;
+  {
+    digitalWrite(LedsPin[idx_led_loop], LOW);
+    idx_led_loop++; 
+    led_status = true;
     if(idx_led_loop > 3)
-      idx_led_loop=0;
+    {
+      idx_led_loop=0; 
+    }
+    old_time = millis();
+  }
+
 }
